d1fd830fc0db464b817172694d168a8995be70fc
gerardo2810 <a20206089@pucp.edu.pe>
2024-11-23 21:01:33 -0500

    pp

--- app/src/main/java/com/example/myapplication/TransportBusAdapter.java
+++ app/src/main/java/com/example/myapplication/TransportBusAdapter.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -64,5 +65,6 @@ public class TransportBusAdapter extends RecyclerView.Adapter<TransportBusAdapte
 
             btnEdit.setOnClickListener(v -> listener.onEditClick(bus));
         }
+
     }
 }
